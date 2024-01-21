f6d5c2dd57251c673c26a3478f6808ac21aa096a
Gabriel Viana <53443525+GabrielViana-Dev@users.noreply.github.com>
2024-01-21 19:14:12 -0300

    api_tempo.py

--- api_tempo.py
+++ api_tempo.py
@@ -1,6 +1,6 @@
 import requests
 
-def consulta_api(city):
+def query(city):
     API_KEY = "886705b4c1182eb1c69f28eb8c520e20"
 
     link = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=pt_br'
